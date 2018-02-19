238991957bed43eab6e490dda1d1f6fa4c8bc6e9
RashelG2020 <35530436+RashelG2020@users.noreply.github.com>
2018-02-19 12:43:10 -0500

    completed changes
    changed the patterns color up a bit

--- make_mask.py
+++ make_mask.py
@@ -1,3 +1,38 @@
+# -*- coding: utf-8 -*-
+'''
+Rashel_G_1_4_3: Change pixels in an image.
+'''
+import matplotlib.pyplot as plt
+import os.path
+import numpy as np # “as” lets us use standard abbreviations
+
+'''Read the image data'''
+# Get the directory of this python script
+directory = os.path.dirname(os.path.abspath(__file__))
+# Build an absolute filename from directory + filename
+filename = os.path.join(directory,'woman.jpg')
+# Read the image data into an array
+img = plt.imread(filename)
+
+'''Show the image data'''
+# Create figure with 1 subplot
+fig, ax = plt.subplots(1, 1)
+# Show the image data in a subplot
+ax.imshow(img, interpolation='none')
+# Show the figure on the screen
+###
+# Make a rectangle of pixels yellow
+###
+
+height = len(img)
+width = len(img[0])
+for row in range(200, 220):
+    for column in range(50,100):
+        img[row][column] = [255, 255, 0] # red + green = yellow
+fig.show()
+
+
+# -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 import numpy as np
 import PIL
@@ -18,11 +53,11 @@ def make_mask(rows, columns, stripe_width):
                 # The % 2 says whether it is an even or odd stripe
                 
                 # Even stripe
-                image[row][column] = [255, 127, 127, 0] # pale red, alpha=0
+                image[row][column] = [500, 150, 200, 700] # pale red, alpha=0
             
             else:
                 # Odd stripe
-                image[row][column] = [255, 0, 255, 255] # magenta, alpha=255
+                image[row][column] = [370, 80, 90, 400] # magenta, alpha=255
     return image
     
 if __name__ == "__main__":
@@ -31,4 +66,3 @@ if __name__ == "__main__":
     fig, ax = plt.subplots(1, 1)
     ax.imshow(image)
     fig.show()            
-              
